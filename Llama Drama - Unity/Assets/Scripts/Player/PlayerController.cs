4cc0ae12bc50f9c0ffe555dbfa3518a069cfd00b
Ryan O'Donnell <ryanodonne@gmail.com>
2018-12-08 18:51:27 -0500

    Added player controller and small test stage.

---
+++ Llama Drama - Unity/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerController : PhysicsObject
+{
+    public float jumpStrength;
+    public float maxSpeed;
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpStrength;
+        }
+
+        if (!grounded)
+        {
+            targetVelocity = move * (maxSpeed * 0.66f);
+        }
+        else if (grounded)
+        {
+            targetVelocity = move * maxSpeed;
+        }
+    }
+
+}
---
+++ Llama Drama - Unity/Assets/Scripts/Player/PlayerStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerStatus
+{
+    public static int HealthPoints { get; set; }
+    public static int MaxHealthPoints { get; set; }
+    public static int CurrencyPoints { get; set; }
+    public static Transform position;
+
+}
